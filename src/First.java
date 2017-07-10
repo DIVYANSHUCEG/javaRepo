2a90fe61718c099e1659720eb4e2c7ea73eb81e0
Divyanshu Kumar <divyanshu.kumar@accoliteindia.com>
2017-07-10 21:45:50 +0530

    Added method for first name and last name

--- src/First.java
+++ src/First.java
@@ -1,6 +1,17 @@
 
 public class First {
+	private static String FIRST_NAME = "Divyanshu";
+	private static String LAST_NAME = "Kumar";
+	
+	private static  String getFirstName(){
+		return FIRST_NAME;
+	}
+	
+	private static  String getLastName(){
+		return LAST_NAME;
+	}
+	
 	public static void main(String... args){
-		System.out.println("Hello");
+		System.out.println(getFirstName()+"  "+getLastName());
 	}
 }
